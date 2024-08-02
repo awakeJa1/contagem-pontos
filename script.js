63655aef8dea86897a583a9a70f3e4ccd612a08d
awakeJa1 <joaoielen@gmail.com>
2024-08-02 19:52:10 -0300

    att

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const teamColor = document.getElementById("team-color").value;
             const teamPoints = parseInt(document.getElementById("team-points").value, 10);
             const teams = await getTeams();
-            teams.push({ name: teamName, color: teamColor, points: teamPoints });
+            teams.push({ Equipe: teamName, Cor: teamColor, Pontos: teamPoints });
             await saveTeams(teams);
             window.location.href = "index.html";
         });
@@ -18,15 +18,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function loadRanking() {
     const teams = await getTeams();
-    teams.sort((a, b) => b.points - a.points);  // Ordena as equipes por pontos (decrescente)
+    teams.sort((a, b) => b.Pontos - a.Pontos);  // Ordena as equipes por pontos (decrescente)
     const tbody = document.querySelector("#ranking-table tbody");
     tbody.innerHTML = "";
     teams.forEach((team, index) => {
         const tr = document.createElement("tr");
         tr.innerHTML = 
-            `<td>${team.name}</td>
-            <td><div style="background-color: ${team.color}; width: 20px; height: 20px;"></div></td>
-            <td>${team.points}</td>
+            `<td>${team.Equipe}</td>
+            <td><div style="background-color: ${team.Cor}; width: 20px; height: 20px;"></div></td>
+            <td>${team.Pontos}</td>
             <td>
                 <button onclick="updateTeamPoints(${index})">Alterar Pontos</button>
                 <button onclick="deleteTeam(${index})">Excluir</button>
@@ -37,9 +37,9 @@ async function loadRanking() {
 
 async function updateTeamPoints(index) {
     const teams = await getTeams();
-    const newPoints = prompt("Insira os novos pontos para a equipe:", teams[index].points);
+    const newPoints = prompt("Insira os novos pontos para a equipe:", teams[index].Pontos);
     if (newPoints !== null) {
-        teams[index].points = parseInt(newPoints, 10);
+        teams[index].Pontos = parseInt(newPoints, 10);
         await saveTeams(teams);
         loadRanking();
     }
