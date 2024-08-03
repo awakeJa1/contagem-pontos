d17a85f0f9d135512eb2e228394594e53384d5ed
awakeJa1 <joaoielen@gmail.com>
2024-08-02 21:39:01 -0300

    att

--- cadastro.js
+++ cadastro.js
@@ -1,32 +1,33 @@
-// cadastro.js
+import { db } from './firebase-config.js';
+import { collection, addDoc } from 'firebase/firestore';
+
 async function addTeam(event) {
-    event.preventDefault();
-    console.log('Formulário de cadastro enviado');
-  
-    const name = document.querySelector('#team-name').value;
-    const color = document.querySelector('#team-color').value;
-    const points = parseInt(document.querySelector('#team-points').value, 10);
-  
-    console.log('Nome da Equipe:', name);
-    console.log('Cor da Equipe:', color);
-    console.log('Pontos:', points);
-  
-    try {
-      await db.collection('teams').add({
-        name: name,
-        color: color,
-        points: points
-      });
-      console.log('Equipe cadastrada com sucesso');
-  
-      document.querySelector('#team-form').reset();
-      window.location.href = 'index.html';
-    } catch (error) {
-      console.error('Erro ao cadastrar equipe:', error);
-    }
+  event.preventDefault();
+  console.log('Formulário de cadastro enviado');
+
+  const name = document.querySelector('#team-name').value;
+  const color = document.querySelector('#team-color').value;
+  const points = parseInt(document.querySelector('#team-points').value, 10);
+
+  console.log('Nome da Equipe:', name);
+  console.log('Cor da Equipe:', color);
+  console.log('Pontos:', points);
+
+  try {
+    await addDoc(collection(db, 'teams'), {
+      name: name,
+      color: color,
+      points: points
+    });
+    console.log('Equipe cadastrada com sucesso');
+
+    document.querySelector('#team-form').reset();
+    window.location.href = 'index.html';
+  } catch (error) {
+    console.error('Erro ao cadastrar equipe:', error);
   }
-  
-  if (document.querySelector('#team-form')) {
-    document.querySelector('#team-form').addEventListener('submit', addTeam);
-  }
-  
+}
+
+if (document.querySelector('#team-form')) {
+  document.querySelector('#team-form').addEventListener('submit', addTeam);
+}
