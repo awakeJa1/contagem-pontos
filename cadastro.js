b4609eb8777ee06ccd5134a2cb6339b9f089ea06
awakeJa1 <joaoielen@gmail.com>
2024-08-02 21:33:52 -0300

    att

--- cadastro.js
+++ cadastro.js
@@ -1,18 +1,29 @@
 // cadastro.js
 async function addTeam(event) {
     event.preventDefault();
+    console.log('Formulário de cadastro enviado');
+  
     const name = document.querySelector('#team-name').value;
     const color = document.querySelector('#team-color').value;
     const points = parseInt(document.querySelector('#team-points').value, 10);
   
-    await db.collection('teams').add({
-      name: name,
-      color: color,
-      points: points
-    });
+    console.log('Nome da Equipe:', name);
+    console.log('Cor da Equipe:', color);
+    console.log('Pontos:', points);
   
-    document.querySelector('#team-form').reset();
-    window.location.href = 'index.html';
+    try {
+      await db.collection('teams').add({
+        name: name,
+        color: color,
+        points: points
+      });
+      console.log('Equipe cadastrada com sucesso');
+  
+      document.querySelector('#team-form').reset();
+      window.location.href = 'index.html';
+    } catch (error) {
+      console.error('Erro ao cadastrar equipe:', error);
+    }
   }
   
   if (document.querySelector('#team-form')) {
