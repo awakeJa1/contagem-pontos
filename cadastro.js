c0f8e01e85ffd4db847268599790a7fcb4959285
awakeJa1 <joaoielen@gmail.com>
2024-08-02 21:43:25 -0300

    att

--- cadastro.js
+++ cadastro.js
@@ -2,32 +2,27 @@ import { db } from './firebase-config.js';
 import { collection, addDoc } from 'firebase/firestore';
 
 async function addTeam(event) {
-  event.preventDefault();
-  console.log('Formulário de cadastro enviado');
+    event.preventDefault();  // Evita o comportamento padrão de envio do formulário
 
-  const name = document.querySelector('#team-name').value;
-  const color = document.querySelector('#team-color').value;
-  const points = parseInt(document.querySelector('#team-points').value, 10);
+    const name = document.querySelector('#team-name').value;
+    const color = document.querySelector('#team-color').value;
+    const points = parseInt(document.querySelector('#team-points').value, 10);
 
-  console.log('Nome da Equipe:', name);
-  console.log('Cor da Equipe:', color);
-  console.log('Pontos:', points);
+    try {
+        await addDoc(collection(db, 'teams'), {
+            name: name,
+            color: color,
+            points: points
+        });
+        console.log('Equipe cadastrada com sucesso');
 
-  try {
-    await addDoc(collection(db, 'teams'), {
-      name: name,
-      color: color,
-      points: points
-    });
-    console.log('Equipe cadastrada com sucesso');
-
-    document.querySelector('#team-form').reset();
-    window.location.href = 'index.html';
-  } catch (error) {
-    console.error('Erro ao cadastrar equipe:', error);
-  }
+        document.querySelector('#team-form').reset();
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.error('Erro ao cadastrar equipe:', error);
+    }
 }
 
 if (document.querySelector('#team-form')) {
-  document.querySelector('#team-form').addEventListener('submit', addTeam);
+    document.querySelector('#team-form').addEventListener('submit', addTeam);
 }
